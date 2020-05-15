3391a562377a47b957ca8dcd0141017d105ab161
Hamidreza Karimi <hkarimi561@gmail.com>
2020-05-15 16:58:50 +0430

    update gcphone updated

--- resources/gcphone/client/client.lua
+++ resources/gcphone/client/client.lua
@@ -11,7 +11,7 @@ local KeyToucheCloseEvent = {
   { code = 176, event = 'Enter' },
   { code = 177, event = 'Backspace' },
 }
-local KeyOpenClose = 288 -- F1
+local KeyOpenClose = 288 -- F2
 local KeyTakeCall = 38 -- E
 local menuIsOpen = false
 local contacts = {}
@@ -46,8 +46,7 @@ end
   Ouverture du téphone lié a un item
   Un solution ESC basé sur la solution donnée par HalCroves
   https://forum.fivem.net/t/tutorial-for-gcphone-with-call-and-job-message-other/177904
---]]
---[[
+
 ESX = nil
 Citizen.CreateThread(function()
 	while ESX == nil do
@@ -65,8 +64,7 @@ end
 function ShowNoPhoneWarning () 
   if (ESX == nil) then return end
   ESX.ShowNotification("Vous n'avez pas de ~r~téléphone~s~")
-end
---]]
+end --]] 
 
 
 --====================================================================================
@@ -755,7 +753,7 @@ RegisterNUICallback('takePhoto', function(data, cb)
         local resp = json.decode(data)
         DestroyMobilePhone()
         CellCamActivate(false, false)
-        cb(json.encode({ url = resp.files[1].url }))   
+        --cb(json.encode({ url = resp.files[1].url }))   
       end)
       takePhoto = false
 		end
